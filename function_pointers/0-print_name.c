b4d833b0b31b239dff7106921f9b7236282ae774
Kolton Rhodes <gamerlife467@gmail.com>
2024-02-21 08:48:35 -0600

    redo

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
 #include "function_pointers.h"
-
 /**
- * print_name - prints a name
- * @name: string name
- * @f: function pointer to print name
- * Return: void
- */
+ * print_name - prints it name
+ * @name: Pointer to name
+ * @f: pointer to function
+ * Return: None
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (!name || !f)
+	{
+		return;
+	}
+	f(name);
 }
